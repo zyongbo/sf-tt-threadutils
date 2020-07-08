37da584761ce90cab95c702002ce0bad98b24be8
Simon Roberts <simon@dancingcloudservices.com>
2020-07-08 09:01:34 -0600

    Break

---
+++ src/main/java/atomicity/Incrementer.java
@@ -0,0 +1,39 @@
+package atomicity;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.LongAdder;
+
+public class Incrementer {
+//  static long count = 0;
+//  static AtomicLong count = new AtomicLong();
+  static LongAdder count = new LongAdder();
+
+  public static void main(String[] args) throws Throwable {
+    Runnable r = () -> {
+      for (int i = 0; i < 100_000_000; i++) {
+        count.increment();
+        count.longValue();
+//        count.incrementAndGet();
+//        synchronized (Incrementer.class) {
+//          count++; // read-modify-write cycle!!!
+//        }
+      }
+    };
+
+    long start = System.nanoTime();
+    List<Thread> threads = new LinkedList<>();
+    for (int i = 0; i < 10; i++) {
+      Thread t1 = new Thread(r);
+      threads.add(t1);
+      t1.start();
+    }
+    for (Thread t : threads) {
+      t.join();
+    }
+    long time = System.nanoTime() - start;
+    System.out.println("Count is " + count.longValue() /*count.get()*/ +
+        " time was " + (time / 1_000_000_000.0));
+  }
+}
